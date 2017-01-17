897722c3573fec4b00ec6dd8bbcdc10b2530ddcc
antoine.leveugle <antoine.leveugle@gmail.com>
2017-01-17 22:08:56 +0100

    fix: fix challenge

--- src/challenges/easy/3_append_next_line.js
+++ src/challenges/easy/3_append_next_line.js
@@ -9,10 +9,10 @@ let input =
 "longitude": 3.082604`;
 
 let output =
-`{"latitude": 50.6806344,"longitude": 3.0818817}
+`{"latitude": 50.6816207,"longitude": 3.0811608}
+{"latitude": 50.6806344,"longitude": 3.0818817}
 {"latitude": 50.679929,"longitude": 3.082089}
-{"latitude": 50.679256,"longitude": 3.082604}
-{"latitude": 50.6781818,"longitude": 3.0822446}`;
+{"latitude": 50.679256,"longitude": 3.082604}`;
 
 module.exports = {
     subject: "Le but de ce challenge est d'expérimenter le travail sur de multiples lignes, en rassemblant sur une ligne les coordonnées GPS qui sont à la base sur deux lignes, et en les changeant de format",
